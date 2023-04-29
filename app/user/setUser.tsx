9cb654821e61f8a336d33da31ecd6030595f8e8a
Jarrison Cano <jarrison.cano@hotmail.com>
2023-04-28 19:20:26 -0500

    Fix error with HEIF images

--- app/user/setUser.tsx
+++ app/user/setUser.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'expo-router'
 import { routes } from '../../utils/routes'
 import { useAppSelector } from '../../store'
+import { useUser } from '../../hooks/useUser'
 import { TouchableOpacity } from 'react-native'
 import { useFetchUpdateUser } from '../../services/user'
 import { Ionicons, AntDesign } from '@expo/vector-icons'
@@ -16,9 +17,10 @@ interface Form {
 }
 
 export default function SetUser () {
-  const { isLoading, mutate } = useFetchUpdateUser()
-  const user = useAppSelector((state) => state.userSlice.user)
+  const { setUser } = useUser()
   const router = useRouter()
+  const user = useAppSelector((state) => state.userSlice.user)
+  const { isSuccess, isLoading, mutate, data, isError, error } = useFetchUpdateUser()
   const {
     control,
     reset,
@@ -28,25 +30,30 @@ export default function SetUser () {
     formState: { errors }
   } = useForm<Form>({
     defaultValues: {
-      description: ''
+      description: '',
+      photoURL: ''
     }
   })
-
-  const secondaryText = !user?.avatar || !user?.description ? 'Complete your profile' : 'Update your profile'
+  const firstTime = !user?.avatar || !user?.description
+  const secondaryText = firstTime ? 'Complete your profile' : 'Update your profile'
 
   const uploadImage = async () => {
     const result = await launchImageLibraryAsync({
       mediaTypes: MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [1, 1],
+      aspect: [4, 3],
       quality: 1
+
     })
 
-    const image = result?.assets?.[0]
-
-    if (!result.canceled && image) {
-      const uri = image.uri
-      setValue('photoURL', uri)
+    if (!result.canceled) {
+      const img = result.assets[0]
+      const isJPG = img.fileName?.endsWith('.jpg') ?? img.fileName?.endsWith('.jpeg')
+      if (!isJPG) {
+        alert('Only JPG images are supported')
+        return
+      }
+      setValue('photoURL', img.uri)
     }
   }
 
@@ -55,9 +62,17 @@ export default function SetUser () {
     if (!user) return
 
     mutate({ ...user, avatar: photoURL, description })
-    reset()
-    router.push(routes.chat)
   }
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      router.push(routes.chat)
+      setUser(data)
+      reset()
+    } else if (isError) {
+      console.error(error)
+    }
+  }, [isSuccess])
 
   useEffect(() => {
     if (user?.description) setValue('description', user?.description)
@@ -65,71 +80,78 @@ export default function SetUser () {
   }, [user])
 
   return (
-        <View pt='20' justifyContent='flex-start'>
-            <Box>
-                <Text color='white' fontSize='2xl' fontWeight='bold'>
-                    Profile Settings 🛠
-                </Text>
-                <Text color='blueGray.400' fontSize='sm'>
-                    {secondaryText}
-                </Text>
-            </Box>
-            <Box mt='12' alignItems='center'>
-                <TouchableOpacity
-                    onPress={() => {
-                      uploadImage().catch(() => { })
-                    }}
-                >
-                    <Avatar bgColor='blueGray.600' size='2xl'>
-                        {watch('photoURL')
-                          ? (
-                                <Image
-                                    width={'full'}
-                                    height={'full'}
-                                    borderRadius='full'
-                                    source={{ uri: watch('photoURL') }}
-                                    alt='Profile picture'
-                                />
-                            )
-                          : (
-                                <AntDesign name='user' size={34} color='white' />
-                            )}
-                        <Avatar.Badge bg='blueGray.300' alignItems='center' justifyContent='center'>
-                            <Ionicons name='ios-cloud-upload-outline' size={15} color='gray' />
-                        </Avatar.Badge>
-                    </Avatar>
-                </TouchableOpacity>
-                <Text pt='5' fontSize='2xl' fontWeight='bold'>
-                    {user?.name}
-                </Text>
-                <Text fontSize='md' fontWeight='thin'>
-                    {user?.email}
-                </Text>
-            </Box>
-            <Box pt='12' alignItems='center'>
-                <CustomTextArea
-                    control={control}
-                    name='description'
-                    placeholder='Tell us about yourself'
-                    height={150}
-                    required={false}
-                    error={errors.description}
-                />
-                <Box alignItems='flex-end'>
-                    <Text fontWeight='semibold' fontSize='xs'>
-                        {watch('description').length}/100
-                    </Text>
-                </Box>
-            </Box>
-            <Button
-                mt='10'
-                isLoading={isLoading}
-                onTouchEnd={() => handleSubmit(onSubmit)().catch}
+    <View pt='20' justifyContent='flex-start'>
+      <Box flexDirection='row' justifyContent='space-between' >
+        <Box width='2/3'>
+          <Text color='white' fontSize='2xl' fontWeight='bold'>
+            Profile Settings 🛠
+          </Text>
+          <Text color='blueGray.400' fontSize='sm'>
+            {secondaryText}
+          </Text>
+        </Box>
+        <Box width='25px'>
+            <TouchableOpacity
+              onPress={() => {
+                router.push(routes.chat)
+              }}
             >
-                <Text fontWeight='bold' textAlign='center'>
-                    Let's start
-                </Text>
-            </Button>
-        </View>
+              <Ionicons name='ios-close' size={25} color='white' />
+            </TouchableOpacity>
+        </Box>
+      </Box>
+      <Box mt='12' alignItems='center'>
+        <TouchableOpacity
+          onPress={() => {
+            uploadImage().catch(() => { })
+          }}
+        >
+          <Avatar bgColor='blueGray.600' size='2xl'>
+            {watch('photoURL')
+              ? (
+                <Image
+                  width={'full'}
+                  height={'full'}
+                  borderRadius='full'
+                  source={{ uri: watch('photoURL') }}
+                  alt='Profile picture'
+                />
+                )
+              : (
+                <AntDesign name='user' size={34} color='white' />
+                )}
+            <Avatar.Badge bg='blueGray.300' alignItems='center' justifyContent='center'>
+              <Ionicons name='ios-cloud-upload-outline' size={15} color='gray' />
+            </Avatar.Badge>
+          </Avatar>
+        </TouchableOpacity>
+        <Text pt='5' fontSize='2xl' fontWeight='bold'>
+          {user?.name}
+        </Text>
+        <Text fontSize='md' fontWeight='thin'>
+          {user?.email}
+        </Text>
+      </Box>
+      <Box pt='12' alignItems='center'>
+        <CustomTextArea
+          control={control}
+          name='description'
+          placeholder='Tell us about yourself'
+          height={150}
+          required={false}
+          error={errors.description}
+        />
+        <Box alignItems='flex-end'>
+          <Text fontWeight='semibold' fontSize='xs'>
+            {watch('description').length}/100
+          </Text>
+        </Box>
+      </Box>
+      <Button mt='10' isLoading={isLoading} onTouchEnd={() => handleSubmit(onSubmit)().catch}>
+        <Text fontWeight='bold' textAlign='center'>
+          Let's start
+        </Text>
+      </Button>
+    </View>
   )
 }
