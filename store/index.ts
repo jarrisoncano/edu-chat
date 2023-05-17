f2e7752a13cb5d65ffee7d28eb69bfd7faf7818f
Jarrison Cano <jarrison.cano@hotmail.com>
2023-05-17 16:43:42 -0500

    Add defaultMidleware redux

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,19 @@
 import userSlice from './user/userSlice'
+import groupsSlice from './groups/groupsSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
-  userSlice
-
+  userSlice,
+  groupsSlice
 })
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
