c466c5f11b9f7e9aa4fc8214262996a410b3cdfe
Jarrison Cano <jarrison.cano@hotmail.com>
2023-05-17 16:44:03 -0500

    Show Menubar only in main screens

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,11 +1,19 @@
-import { Slot } from 'expo-router'
+import { routes } from '../../utils/routes'
+import { Slot, usePathname } from 'expo-router'
 import { MenuBar } from '../../components/shared/MenuBar'
+import { useListenUserGroupsChanges } from '../../services/chat'
 
-export default function MainLayout () {
+export default function MainLayout() {
+  useListenUserGroupsChanges()
+  const pathname = usePathname()
+  const validPaths = [routes.home, routes.settings, routes.events]
+
   return (
-        <>
-            <Slot />
-            <MenuBar />
-        </>
+    <>
+      <Slot />
+      {
+        validPaths.includes(pathname) && <MenuBar />
+      }
+    </>
   )
 }
