a4c06cb8d6f0c5a2bf81717529c300724518d23c
Jarrison Cano <jarrison.cano@hotmail.com>
2023-05-19 17:09:12 -0500

    Fix validation of imageUpload

--- components/utils/getImage.ts
+++ components/utils/getImage.ts
@@ -1,23 +1,23 @@
 import { MediaTypeOptions, launchImageLibraryAsync } from 'expo-image-picker'
 
 export const getImageFromLibary = async () => {
-  const result = await launchImageLibraryAsync({
-    mediaTypes: MediaTypeOptions.Images,
-    allowsEditing: true,
-    aspect: [4, 3],
-    quality: 1
-  })
+	const result = await launchImageLibraryAsync({
+		mediaTypes: MediaTypeOptions.Images,
+		allowsEditing: true,
+		aspect: [4, 3],
+		quality: 1
+	})
 
-  if (!result.canceled) {
-    const img = result.assets[0]
-    const fileNameLower = img.fileName?.toLowerCase()
+	if (!result.canceled) {
+		const img = result.assets[0]
+		const fileNameLower = img.fileName?.toLowerCase()
 
-    const isJPG = fileNameLower?.endsWith('.jpg') ?? fileNameLower?.endsWith('.jpeg')
-    if (!isJPG) {
-      alert('Only JPG images are supported')
-      return
-    }
-    return img
-  }
-  return null
+		const isJPG = fileNameLower?.endsWith('.jpg') || fileNameLower?.endsWith('.jpeg')
+		if (!isJPG) {
+			alert('Only JPG images are supported')
+			return
+		}
+		return img
+	}
+	return null
 }
