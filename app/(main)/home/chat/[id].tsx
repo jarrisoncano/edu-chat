4b575ee19c19b0ffd6a9ed7fce086e68e4d5d75d
Jarrison Cano <jarrison.cano@hotmail.com>
2023-05-19 18:50:06 -0500

    remove background image in chat

--- app/(main)/home/chat/[id].tsx
+++ app/(main)/home/chat/[id].tsx
@@ -40,64 +40,56 @@ export default function Chat(): JSX.Element {
 	return (
 		<View pt='16' bg='blueGray.900' px='0'>
 			<ChatHeader group={group} />
-			<ImageBackground
-				resizeMode='cover'
-				style={{
-					height: '100%'
-				}}
-				source={{ uri: assets?.[0].uri ?? undefined }}
-			>
-				<Box px='5' h='full'>
-					<ScrollView py='2' maxH='4/5' flexDir='column-reverse'>
-						{chat.map((message, index) => (
-							<ChatMessage key={index} message={message} />
-						))}
-					</ScrollView>
-					<Box
-						h='16'
-						flexDir='row'
-						borderRadius='2xl'
-						justifyContent='space-between'
-						alignItems='center'
-						bg='blueGray.900'
-						px='3'
+			<Box bg='blueGray.800' px='5' h='full'>
+				<ScrollView py='2' maxH='4/5' flexDir='column-reverse'>
+					{chat.map((message, index) => (
+						<ChatMessage key={index} message={message} />
+					))}
+				</ScrollView>
+				<Box
+					h='16'
+					flexDir='row'
+					borderRadius='2xl'
+					justifyContent='space-between'
+					alignItems='center'
+					bg='blueGray.900'
+					px='3'
+				>
+					<CustomInput
+						name='message'
+						control={control}
+						variant='unstyled'
+						height={10}
+						style={{ width: '85%', backgroundColor: 'blueGray.800' }}
+						backgroundColor='blueGray.900'
+						error={undefined}
+						type='text'
+						placeholder='Message...'
+						label=''
+					/>
+					<TouchableOpacity
+						onPress={() => {
+							handleSubmit(onSubmit)()
+						}}
 					>
-						<CustomInput
-							name='message'
-							control={control}
-							variant='unstyled'
-							height={10}
-							style={{ width: '85%', backgroundColor: 'blueGray.800' }}
-							backgroundColor='blueGray.900'
-							error={undefined}
-							type='text'
-							placeholder='Message...'
-							label=''
-						/>
-						<TouchableOpacity
-							onPress={() => {
-								handleSubmit(onSubmit)()
-							}}
+						<Box
+							bgColor='blueGray.700'
+							width='10'
+							h='10'
+							flexDir='row'
+							borderRadius='lg'
+							justifyContent='center'
+							alignItems='center'
 						>
-							<Box
-								bgColor='blueGray.700'
-								width='10'
-								h='10'
-								flexDir='row'
-								borderRadius='lg'
-								justifyContent='center'
-								alignItems='center'
-							>
-								{isLoading ? (
-									<Spinner color='indigo.400' />
-								) : (
-									<Feather name='send' size={20} color='#818cf8' />
-								)}
-							</Box>
-						</TouchableOpacity>
-					</Box>
+							{isLoading ? (
+								<Spinner color='indigo.400' />
+							) : (
+								<Feather name='send' size={20} color='#818cf8' />
+							)}
+						</Box>
+					</TouchableOpacity>
 				</Box>
-			</ImageBackground>
+			</Box>
 		</View>
 	)
 }
