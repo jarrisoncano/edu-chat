ae4278f0e51a6b39c4f60e7bdb22e03b2d89ba7e
Jarrison Cano <jarrison.cano@hotmail.com>
2023-05-21 00:44:07 -0500

    change button _pressed bgColor

--- utils/customTheme.ts
+++ utils/customTheme.ts
@@ -1,40 +1,43 @@
 import { extendTheme } from 'native-base'
 
 export const customTheme = extendTheme({
-  components: {
-    View: {
-      defaultProps: {
-        px: '5',
-        flex: 1,
-        pt: '16',
-        width: 'full',
-        height: 'full',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        bg: 'blueGray.800'
-      }
-    },
-    Text: {
-      defaultProps: {
-        color: 'white'
-      }
-    },
-    Box: {
-      defaultProps: {
-        width: 'full'
-      }
-    },
-    Button: {
-      defaultProps: {
-        bg: 'indigo.400',
-      }
-    },
-    Input: {
-      defaultProps: {
-        _focus: {
-          borderColor: 'indigo.400',
-        }
-      }
-    }
-  }
+	components: {
+		View: {
+			defaultProps: {
+				px: '5',
+				flex: 1,
+				pt: '16',
+				width: 'full',
+				height: 'full',
+				justifyContent: 'flex-start',
+				alignItems: 'center',
+				bg: 'blueGray.800'
+			}
+		},
+		Text: {
+			defaultProps: {
+				color: 'white'
+			}
+		},
+		Box: {
+			defaultProps: {
+				width: 'full'
+			}
+		},
+		Button: {
+			defaultProps: {
+				bg: 'indigo.400',
+				_pressed: {
+					bg: 'indigo.500'
+				}
+			}
+		},
+		Input: {
+			defaultProps: {
+				_focus: {
+					borderColor: 'indigo.400'
+				}
+			}
+		}
+	}
 })
